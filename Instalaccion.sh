f5ac7a6be60cb079a448fa7c3dfe0b0ffd5fec1c
Pedro Barquin Ayuso <pedro.barquin@gmail.com>
2017-02-23 10:26:32 +0100

    Correccion error instalaccion

--- Instalaccion.sh
+++ Instalaccion.sh
@@ -5,7 +5,7 @@ sudo apt-get install python-picamera -y
 mv Si.mp3 No.mp3 PreguntasRespuestas.csv EnvioCorreo.py Demo7.py camara.py lanzar.sh lanzar1.sh /home/pi/Desktop/
 sudo mv script/detector-init script/detector-init1  /etc/init.d/
 sudo chmod 775 /etc/init.d/detector-init
-sudo chmod 7777 /etc/init.d/detector-init1
+sudo chmod 775 /etc/init.d/detector-init1
 sudo sudo update-rc.d detector-init defaults
 sudo sudo update-rc.d detector-init1 defaults
 cd /home/pi/Desktop/
