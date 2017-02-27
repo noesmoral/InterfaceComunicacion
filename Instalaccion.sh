9bbadd9d5045d6a0e121e3b7190b25b9d4672f1e
Pedro Barquin Ayuso <pedro.barquin@gmail.com>
2017-02-27 22:03:39 +0100

    Subida error instalaccion

--- Instalaccion.sh
+++ Instalaccion.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
-sudo apt-get install espeak
+sudo apt-get install espeak -y
 sudo apt-get install python-picamera -y
-sudo apt-get install python-opencv
-sudo apt-get install python-scipy
-sudo apt-get install ipython
-sudo apt-get install qrencode
-sudo apt-get install python-zbar
+sudo apt-get install python-opencv -y
+sudo apt-get install python-scipy -y
+sudo apt-get install ipython -y
+sudo apt-get install qrencode -y
+sudo apt-get install python-zbar -y 
+sudo apt-get install alsa-utils -y
+sudo apt-get install mpg321 -y
+sudo apt-get install lame -y
 
-mv Si.mp3 No.mp3 PreguntasRespuestas.csv EnvioCorreo.py Demo7.py camara.py lanzar.sh lanzar1.sh /home/pi/Desktop/
+sudo mv Si.mp3 No.mp3 PreguntasRespuestas.csv EnvioCorreo.py Demo7.py camara.py lanzar.sh lanzar1.sh /home/pi/Desktop/
+sudo mv pictogramas/ /home/pi/Desktop/
 sudo mv script/detector-init script/detector-init1  /etc/init.d/
 sudo chmod 775 /etc/init.d/detector-init
 sudo chmod 775 /etc/init.d/detector-init1
-sudo sudo update-rc.d detector-init defaults
-sudo sudo update-rc.d detector-init1 defaults
+sudo update-rc.d detector-init defaults
+sudo update-rc.d detector-init1 defaults
 cd /home/pi/Desktop/
 mkdir Capturas PreguntasMP3 RespuestasMP3
