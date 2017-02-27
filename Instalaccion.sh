3ca7cd311229b47595355ee00044d996be1b4118
Pedro Barquin Ayuso <pedro.barquin@gmail.com>
2017-02-27 21:01:34 +0100

    Resubida

--- Instalaccion.sh
+++ Instalaccion.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 sudo apt-get install espeak
 sudo apt-get install python-picamera -y
+sudo apt-get install python-opencv
+sudo apt-get install python-scipy
+sudo apt-get install ipython
+sudo apt-get install qrencode
+sudo apt-get install python-zbar
 
 mv Si.mp3 No.mp3 PreguntasRespuestas.csv EnvioCorreo.py Demo7.py camara.py lanzar.sh lanzar1.sh /home/pi/Desktop/
 sudo mv script/detector-init script/detector-init1  /etc/init.d/
